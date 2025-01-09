b98ebc3c51b34d9cd53af86e63c3b448e0936762
小杜 <1370484311@qq.com>
2025-01-10 00:12:07 +0800

    构建无忧项目1

--- yudao-module-wuyou/yudao-module-wuyou-api/src/main/java/cn/iocoder/yudao/module/wuyou/enums/ErrorCodeConstants.java
+++ yudao-module-wuyou/yudao-module-wuyou-api/src/main/java/cn/iocoder/yudao/module/wuyou/enums/ErrorCodeConstants.java
@@ -3,5 +3,5 @@ package cn.iocoder.yudao.module.wuyou.enums;
 import cn.iocoder.yudao.framework.common.exception.ErrorCode;
 
 public interface ErrorCodeConstants {
-    ErrorCode BASIC_DATA_NOT_EXISTS = new ErrorCode(10001, "无忧基础数据不存在");
+    ErrorCode BASIC_DATA_NOT_EXISTS = new ErrorCode(10002, "无忧基础数据不存在");
 }
